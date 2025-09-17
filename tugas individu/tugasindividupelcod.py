466b76782912441e6e669c1d9d47d0e0d01e654a
Geraldo1302 <tobingaldo198@gmail.com>
2025-09-17 11:16:26 +0700

    Add files via upload

---
+++ tugas individu/tugasindividupelcod.py
@@ -0,0 +1,23 @@
+nama= " geraldo pandapotan lumbantobing "
+umur= 18 
+tinggi= 166 
+angkafavorit= 5
+
+print("nama saya :" , nama)
+print("umur saya :" , umur)
+print("tinggi saya :" , tinggi ,"cm")
+print("angka favorit saya :" , angkafavorit , "\n")
+
+
+pensil=4
+buku=2
+harga_pensil=2000
+harga_buku=5000
+total_belanja=(pensil*harga_pensil)+(buku*harga_buku)
+
+print("total belanja di toko alat tulis : ",total_belanja , "\n")
+
+if angkafavorit % 2 == 0:
+    print("angka favorit adalah genap")
+else:
+    print("angka favorit adalah ganjil")
